be89bb9d3ad181c347d7e4101c0d6963e453976c
omertr04 <omertr04@gmail.com>
2023-07-13 02:12:05 +1000

    changes made to how the code is written

--- script.js
+++ script.js
@@ -1,163 +1,163 @@
-// Quiz Questions - list of all the questions i wanted the questions to make sense so i got generic javascript questions to test the user 
-var questions = [
+// Quiz Questions
+var q = [
   {
-    question: "What is the correct syntax for referring to an external script called 'script.js'?",
-    choices: ["<script href='script.js'>", "<script src='script.js'>", "<script name='script.js'>", "<script file='script.js'>"],
-    answer: "<script src='script.js'>"
+    q: "What is the correct syntax for referring to an external script called 'script.js'?",
+    c: ["<script href='script.js'>", "<script src='script.js'>", "<script name='script.js'>", "<script file='script.js'>"],
+    a: "<script src='script.js'>"
   },
   {
-    question: "Inside which HTML element do we put the JavaScript code?",
-    choices: ["<scripting>", "<javascript>", "<script>", "<js>"],
-    answer: "<script>"
+    q: "Inside which HTML element do we put the JavaScript code?",
+    c: ["<scripting>", "<javascript>", "<script>", "<js>"],
+    a: "<script>"
   },
   {
-    question: "How do you create a function in JavaScript?",
-    choices: ["function: myFunction()", "function myFunction()", "function = myFunction()", "function.myFunction()"],
-    answer: "function myFunction()"
+    q: "How do you create a function in JavaScript?",
+    c: ["function: myFunction()", "function myFunction()", "function = myFunction()", "function.myFunction()"],
+    a: "function myFunction()"
   },
   {
-    question: "Which built-in method removes the last element from an array and returns that element?",
-    choices: ["pop()", "last()", "push()", "remove()"],
-    answer: "pop()"
+    q: "Which built-in method removes the last element from an array and returns that element?",
+    c: ["pop()", "last()", "push()", "remove()"],
+    a: "pop()"
   }
 ];
 
-// Variables - gets the elements from the  html 
-var startPage = document.getElementById('start-page');
-var quizPage = document.getElementById('quiz-page');
-var resultPage = document.getElementById('result-page');
-var startBtn = document.getElementById('start-btn');
-var questionElement = document.getElementById('question');
-var choicesElement = document.getElementById('choices');
-var timerElement = document.getElementById('timer');
-var timeLeftElement = document.getElementById('time-left');
-var scoreElement = document.getElementById('score');
-var highscoreForm = document.getElementById('highscore-form');
-var initialsInput = document.getElementById('initials');
-var goBackBtn = document.getElementById('go-back-btn');
-var clearScoresBtn = document.getElementById('clear-scores-btn');
-var highscoreList = document.getElementById('highscore-list');
+// Variables
+var sp = document.getElementById('start-page');
+var qp = document.getElementById('quiz-page');
+var rp = document.getElementById('result-page');
+var sb = document.getElementById('start-btn');
+var qe = document.getElementById('question');
+var ce = document.getElementById('choices');
+var te = document.getElementById('timer');
+var tle = document.getElementById('time-left');
+var se = document.getElementById('score');
+var hf = document.getElementById('highscore-form');
+var ie = document.getElementById('initials');
+var gb = document.getElementById('go-back-btn');
+var csb = document.getElementById('clear-scores-btn');
+var hl = document.getElementById('highscore-list');
 
-var currentQuestionIndex = 0;
-var timeLeft = 60;
-var score = 0;
-var timer;
+var ci = 0;
+var tl = 60;
+var sc = 0;
+var tmr;
 
-// Start the quiz - begins the quiz 
+// Start the quiz
 function startQuiz() {
-  startPage.classList.add('hide');
-  quizPage.classList.remove('hide');
+  sp.classList.add('hide');
+  qp.classList.remove('hide');
   startTimer();
   showQuestion();
 }
 
-// Start the timer when you click to start the quiz starting at 60 seconds
+// Start the timer when the quiz starts at 60 seconds
 function startTimer() {
-  timer = setInterval(function () {
-    timeLeft--;
-    timeLeftElement.textContent = timeLeft;
+  tmr = setInterval(function () {
+    tl--;
+    tle.textContent = tl;
 
-    if (timeLeft <= 0) {
+    if (tl <= 0) {
       endQuiz();
     }
   }, 1000);
 }
 
-// This part displays the question 
+// Display the question
 function showQuestion() {
-  var question = questions[currentQuestionIndex];
-  questionElement.textContent = question.question;
-  choicesElement.innerHTML = '';
+  var qq = q[ci];
+  qe.textContent = qq.q;
+  ce.innerHTML = '';
 
-  for (var i = 0; i < question.choices.length; i++) {
-    var choice = question.choices[i];
-    var choiceBtn = document.createElement('button');
-    choiceBtn.textContent = choice;
-    choiceBtn.setAttribute('class', 'choice');
-    choiceBtn.setAttribute('value', choice);
-    choiceBtn.onclick = checkAnswer;
-    choicesElement.appendChild(choiceBtn);
+  for (var i = 0; i < qq.c.length; i++) {
+    var ch = qq.c[i];
+    var cb = document.createElement('button');
+    cb.textContent = ch;
+    cb.setAttribute('class', 'choice');
+    cb.setAttribute('value', ch);
+    cb.onclick = checkAnswer;
+    ce.appendChild(cb);
   }
 }
 
-// This part validates the answer to each question 
+// Validate the answer to each question
 function checkAnswer(event) {
-  var selectedAnswer = event.target.value;
-  var question = questions[currentQuestionIndex];
+  var sa = event.target.value;
+  var qq = q[ci];
 
-  if (selectedAnswer === question.answer) {
-    score += 10;
+  if (sa === qq.a) {
+    sc += 10;
   } else {
-    timeLeft -= 10;
+    tl -= 10;
   }
 
-  currentQuestionIndex++;
+  ci++;
 
-  if (currentQuestionIndex === questions.length) {
+  if (ci === q.length) {
     endQuiz();
   } else {
     showQuestion();
   }
 }
 
-// Shows final score 
+// Show the final score
 function endQuiz() {
-  clearInterval(timer);
-  quizPage.classList.add('hide');
-  resultPage.classList.remove('hide');
-  scoreElement.textContent = 'Your final score is ' + score;
+  clearInterval(tmr);
+  qp.classList.add('hide');
+  rp.classList.remove('hide');
+  se.textContent = 'Your final score is ' + sc;
 }
 
-// This function intakes the initials the user puts to display on page when working with local storage we gotta turn eveything into a string for it to function correctly 
+// Handle form submission to store initials and score
 function handleFormSubmit(event) {
   event.preventDefault();
 
-  var initials = initialsInput.value.trim();
+  var ini = ie.value.trim();
 
-  if (initials !== '') {
-    var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
-    var newScore = { initials: initials, score: score };
-    highscores.push(newScore);
-    localStorage.setItem('highscores', JSON.stringify(highscores));
+  if (ini !== '') {
+    var hs = JSON.parse(localStorage.getItem('highscores')) || [];
+    var ns = { initials: ini, score: sc };
+    hs.push(ns);
+    localStorage.setItem('highscores', JSON.stringify(hs));
     renderHighscores();
   }
 }
 
-// Clear highscores - this was a pretty usefull and easy function to add as sometimes the user may want to refresh the highscores to start a new leaderboard
+// Clear highscores
 function clearHighscores() {
   localStorage.removeItem('highscores');
   renderHighscores();
 }
 
-// this is where the highscores are rendered 
+// Render highscores
 function renderHighscores() {
-  var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
-  highscoreList.innerHTML = '';
+  var hs = JSON.parse(localStorage.getItem('highscores')) || [];
+  hl.innerHTML = '';
 
-  for (var i = 0; i < highscores.length; i++) {
-    var highscore = highscores[i];
-    var highscoreItem = document.createElement('li');
-    highscoreItem.textContent = highscore.initials + ' - ' + highscore.score;
-    highscoreList.appendChild(highscoreItem);
+  for (var i = 0; i < hs.length; i++) {
+    var h = hs[i];
+    var hi = document.createElement('li');
+    hi.textContent = h.initials + ' - ' + h.score;
+    hl.appendChild(hi);
   }
 }
 
-// This basically refreshes everything where you can go back and start the quiz again
+// Go back to the start page
 function goBack() {
-  resultPage.classList.add('hide');
-  quizPage.classList.add('hide');
-  startPage.classList.remove('hide');
-  currentQuestionIndex = 0;
-  timeLeft = 60;
-  score = 0;
-  clearInterval(timer);
+  rp.classList.add('hide');
+  qp.classList.add('hide');
+  sp.classList.remove('hide');
+  ci = 0;
+  tl = 60;
+  sc = 0;
+  clearInterval(tmr);
 }
 
-// Event listeners - i catogrised all the event listeners making it easy for me to manage each input a user does 
-startBtn.addEventListener('click', startQuiz);
-highscoreForm.addEventListener('submit', handleFormSubmit);
-goBackBtn.addEventListener('click', goBack);
-clearScoresBtn.addEventListener('click', clearHighscores);
+// Event listeners
+sb.addEventListener('click', startQuiz);
+hf.addEventListener('submit', handleFormSubmit);
+gb.addEventListener('click', goBack);
+csb.addEventListener('click', clearHighscores);
 
-// This Renders highscores on page load
+// Render highscores on page load
 renderHighscores();
